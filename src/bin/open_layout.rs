0604d70c039711dfc7af3e8236f558820b1ca162
David Weis <dweis7@gmail.com>
2021-05-08 21:22:23 +0100

    Open layout can print link

--- src/bin/open_layout.rs
+++ src/bin/open_layout.rs
@@ -1,24 +1,49 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use clap::{AppSettings, Clap};
 use ergodox_driver::driver;
 
+#[derive(Clap)]
+#[clap(version = "0.1", author = "David W <dweis7@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Args {
+    /// Tries to invoke os default browser to open link in
+    /// Otherwise prints link to stdout
+    #[clap(short = 'b', long)]
+    open_browser: bool,
+    /// prints messages to stderr
+    #[clap(short, long)]
+    verbose: bool,
+}
+
 fn main() -> Result<()> {
+    let args = Args::parse();
     let mut device = driver::ErgodoxDriver::connect_to_first()?;
     match device.keyboard_type() {
-        driver::KeyboardType::Ergodox => println!("Detected Ergodox"),
-        _ => println!(
-            "Detected {:?} keyboard, only Ergodox fully supported",
-            device.keyboard_type()
-        ),
+        driver::KeyboardType::Ergodox => {
+            if args.verbose {
+                eprintln!("Detected Ergodox");
+            }
+        }
+        _ => {
+            if args.verbose {
+                eprintln!(
+                    "Detected {:?} keyboard, only Ergodox fully supported",
+                    device.keyboard_type()
+                );
+            }
+        }
     }
     for _ in 0..10 {
         device.write(driver::Command::LandingPage)?;
         for message in device.read()? {
             if let driver::Event::LayoutName(ref layout_id) = message {
-                println!(
-                    "Layout id: {} revision: {}",
-                    layout_id.id(),
-                    layout_id.revision()
-                );
+                if args.verbose {
+                    eprintln!(
+                        "Layout id: {} revision: {}",
+                        layout_id.id(),
+                        layout_id.revision()
+                    );
+                }
                 // TODO (David): The first path element may have to change for other boards
                 // can't test it without them
                 let link = format!(
@@ -26,14 +51,18 @@ fn main() -> Result<()> {
                     layout_id.id(),
                     layout_id.revision()
                 );
-                if webbrowser::open(&link).is_ok() {
-                    println!("Opened {}", link);
+                if args.open_browser {
+                    if webbrowser::open(&link).is_ok() {
+                        eprintln!("Opened {}", link);
+                    } else {
+                        return Err(anyhow!("Failed to open {} in browser", link));
+                    }
                 } else {
-                    eprintln!("Failed to open {}", link);
+                    println!("{}", link);
                 }
                 return Ok(());
             }
         }
     }
-    Ok(())
+    Err(anyhow!("Failed to get layout"))
 }
