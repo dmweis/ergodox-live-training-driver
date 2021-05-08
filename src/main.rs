4b2ccff23d16bacac1ce6ac5aa2f0ba8e9222e49
David Weis <dweis7@gmail.com>
2021-05-08 02:39:57 +0100

    Retru getting layout

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,25 @@ fn main() -> Result<()> {
         ColorChoice::Auto,
     )?;
     let mut device = driver::ErgodoxDriver::connect_to_first()?;
-    device.write(driver::Command::LandingPage)?;
-    std::thread::sleep(std::time::Duration::from_millis(500));
     let mut layout: Option<layout_store_client::Layout> = None;
-    device.read()?.iter().for_each(|message| {
-        if let driver::Event::LayoutName(ref layout_id) = message {
-            layout =
-                layout_store_client::query_layout(layout_id.id.clone(), layout_id.revision.clone())
-                    .ok();
+    loop {
+        for message in device.read()? {
+            if let driver::Event::LayoutName(ref layout_id) = message {
+                layout = layout_store_client::query_layout(
+                    layout_id.id.clone(),
+                    layout_id.revision.clone(),
+                )
+                .ok();
+                info!("Got layout");
+                break;
+            }
+            info!("Received other message: {:?}", message)
         }
-        info!("{:?}", message)
-    });
+        if layout.is_some() {
+            break;
+        }
+        device.write(driver::Command::LandingPage)?;
+    }
     loop {
         std::thread::sleep(std::time::Duration::from_secs(1));
         device.write(driver::Command::Pair)?;
